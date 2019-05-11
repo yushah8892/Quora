2d39ff46b753385206a844d66146d6e09eff912c
shivp1983 <shiv.is.here@gmail.com>
2019-05-11 12:29:28 +0530

    FORTH COMMIT AFTER MODIFICATIONS AND ADDING COMMENTS

--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -13,15 +13,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+//@Service annotation labels a class as a service class
 @Service
 public class AnswerService {
 
+    //Injecting the dependency using @Autowired annotation
     @Autowired
     private AnswerDao answerDao;
 
+    //Injecting the dependency using @Autowired annotation
     @Autowired
     private UserDao userDao;
 
+
+    //@Transactional annotation is used to do all the transactions related to the data base. It simplifies the process of
+    //transacting with a database.
     @Transactional(propagation = Propagation.REQUIRED)
     public AnswerEntity createAnswer(String accessToken, AnswerEntity answerEntity) throws
             InvalidQuestionException, AuthorizationFailedException {
@@ -42,23 +48,27 @@ public class AnswerService {
 
     }
 
-    public AnswerEntity editAnswerContent(String accessToken, AnswerEntity editedAnswerEntity)
+    public AnswerEntity editAnswerContent(String accessToken, String answerId, String content)
             throws AuthorizationFailedException, AnswerNotFoundException {
 
         UserAuthTokenEntity userAuthTokenEntity = userDao.getUserAuthTokenEntity(accessToken);
-        String answerUUID = new AnswerEntity().getUuid();
 
-        if(userAuthTokenEntity.getUser() != editedAnswerEntity.getUserEntity()){
-            throw new AuthorizationFailedException("ATHR-003", "Only the answer owner can edit the answer");
-        } else if (userAuthTokenEntity == null){
-            throw new AuthorizationFailedException("ATHR-001","User has not signed in");
-        } else if(userAuthTokenEntity.getLogoutAt() != null) {
-            throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to post a question");
-        } else if (answerUUID == null){
-            throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
+        if(userAuthTokenEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthTokenEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out. Sign in first to edit an answer");
         } else {
-            return answerDao.editAnswerContent(editedAnswerEntity);
+            AnswerEntity editAnswer = answerDao.getAnswerById(answerId);
+            if(editAnswer == null) {
+                throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
+            } else {
+                if (editAnswer.getUserEntity().getId() != userAuthTokenEntity.getUser().getId()){
+                    throw new AuthorizationFailedException("ATHR-003", "Only the answer owner can edit the answer");
+                } else {
+                    editAnswer.setAnswer(content);
+                    return answerDao.editAnswerContent(editAnswer);
+                }
+            }
         }
-
     }
 }
